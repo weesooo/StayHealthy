23b312773660c597fc715d78f5e00f00b44f5235
Wisam Al Ali <walali2@uic.edu>
2019-04-25 03:44:20 +0000

    more changes

--- js/map.js
+++ js/map.js
@@ -1,73 +1,36 @@
-// Main Javascript for map
-let map;
-// storing markers in array for use later
-let markersArray = [];
-
-let chicago = {
-  lat: 41.8781,
-  lng: -87.6298
-};
-
-const APIurl = "https://data.cityofchicago.org/resource/cdmx-wzbz.json?$limit=50";
-
-// Initialize and add the map
 function initMap() {
-  let map = new google.maps.Map(document.getElementById('map'), {
-    center: chicago,
-    zoom: 11
-  });
+  // The location of Uluru
+  var chicago = {lat: 41.8781, lng: -87.6298};
+  // The map, centered at Uluru
+  var map = new google.maps.Map(
+      document.getElementById('map'), {zoom: 11, center: chicago});
 
-  $.get(APIurl, function(response) {
-    let data = response;
-    createMarkers(map, data);
-  });
+  $.get("https://data.cityofchicago.org/resource/cwig-ma7x.json?$limit=5",
 
-  function createMarkers(map, data) {
-    // let url = "http://maps.google.com/mapfiles/ms/icons/";
-    // url += color + "-dot.png";
-
-    $.each(data, function(i, v) {
-      let marker;
-      let location = {
-        lat: parseFloat(v.latitude),
-        lng: parseFloat(v.longitude)
-      }
-
-      if (v.status === "Open") {
-        marker = new google.maps.Marker({
-          map: map,
-          position: location,
-          icon: {
-            url: "http://maps.google.com/mapfiles/ms/icons/purple-dot.png"
-          }
-        })
-      } 
-        else {
-        marker = new google.maps.Marker({
-          map: map,
-          position: location,
-          icon: {
-            url: "http://maps.google.com/mapfiles/ms/icons/yellow-dot.png"
-          }
-        })
-      }
-
-      let infowindow = new google.maps.InfoWindow({
-        content: 'Ward: ' + v.ward + '<br/>' + 'Status: ' + v.status +
-          '<br/>Address: ' + v.street_address + '<br/>SRN: ' + v.service_request_number + '<br/>Graffiti location: ' + v.where_is_the_graffiti_located_ +
-          '<br/>Type of Surface: ' + v.what_type_of_surface_is_the_graffiti_on_ + '<br/>Date Created: ' + v.creation_date + '<br/>Completion Date: ' + v.completion_date
+      function(response) {
+        console.log("in data callback");
+        var data = response;
+        createMarkers(map, data );
       });
 
-      marker.addListener('click', function(results) {
+}
+
+  function createMarkers (map, data) {
+    console.log(data);
+    $.each(data, function(i,v) {
+
+      var location = {lat: parseFloat(v.latitude), lng: parseFloat(v.longitude) }
+      
+      var marker = new google.maps.Marker({position: location, map: map});
+
+      var infowindow = new google.maps.InfoWindow({
+        content: v.dba_name
+      });
+
+      marker.addListener('click', function() {
         infowindow.open(map, marker);
       });
-        
-      let textBox = document.getElementById("input-search-bar").innerHTML;
-      console.log(textBox);
-      if (textBox.innerHTML > 0) {
-        marker.setMap(null);
-      }
 
     });
+
   }
-};
