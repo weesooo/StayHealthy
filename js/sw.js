473d2c338bacada7cb353206aee374bf1228d8f2
Wisam Al Ali <walali2@uic.edu>
2019-04-28 01:54:39 +0000

    added sw and manifest

---
+++ js/sw.js
@@ -0,0 +1,34 @@
+var cacheName = 'StayHealthyv1';
+var appShellFiles = [
+ "./index.html",
+ "./js/map.js",
+ "./pic2.png",
+ "./style.css",
+];
+
+
+self.addEventListener('install', function(e) {
+  console.log('[Service Worker] Install');
+  e.waitUntil(
+    caches.open(cacheName).then(function(cache) {
+          console.log('[Service Worker] Caching all: app shell and content');
+      return cache.addAll(appShellFiles);
+    })
+  );
+});
+
+
+self.addEventListener('fetch', function(e) {
+  e.respondWith(
+    caches.match(e.request).then(function(r) {
+          console.log('[Service Worker] Fetching resource: '+e.request.url);
+      return r || fetch(e.request).then(function(response) {
+                return caches.open(cacheName).then(function(cache) {
+          console.log('[Service Worker] Caching new resource: '+e.request.url);
+          cache.put(e.request, response.clone());
+          return response;
+        });
+      });
+    })
+  );
+});
